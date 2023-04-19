89db0f7c97d2ec24dd24bcb19e0fdf373d92b7af
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-04-19 15:22:00 -0400

    roll forward

--- goal_app/app/models/user.rb
+++ goal_app/app/models/user.rb
@@ -10,4 +10,29 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+    validates :username, :password, presence: true
+
+
+    attr_reader :password
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        if user && is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    # def 
 end
